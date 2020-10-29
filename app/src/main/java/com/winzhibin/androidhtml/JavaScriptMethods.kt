1af731150c137a9e51dd45141809db1adca92cb2
lieri <lieri0318>
2020-10-30 00:08:50 +0800

    yanhuomatou2015 20201030

---
+++ app/src/main/java/com/winzhibin/androidhtml/JavaScriptMethods.kt
@@ -0,0 +1,59 @@
+package com.winzhibin.androidhtml
+
+import android.content.Context
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.widget.Toast
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
+import org.json.JSONObject
+import java.net.URL
+
+/**
+ * android与h5通信的桥梁类
+ */
+class JavaScriptMethods {
+
+
+    private  var mContext:Context?=null
+    private  var mWebView:WebView?=null
+
+    constructor(context:Context?,webView: WebView?){
+        this.mContext=context
+        this.mWebView=webView
+    }
+
+     //必须加上注解，否则android 4.2之后，不允许h5调用该方法
+     @JavascriptInterface
+     fun showToast(jsonData:String){
+        //Toast.makeText(mContext,jsonData,Toast.LENGTH_LONG).show()
+        // mContext?.toast(jsonData)
+         mContext?.let { it.toast(jsonData) } //it指的是：当前上下文对象
+     }
+
+    @JavascriptInterface
+    fun getrequestData(jsonData:String){
+        var jsJson=JSONObject(jsonData)
+        var callback=jsJson.optString("callback")
+        doAsync {
+            //模拟请求接口数据
+            var url= URL("http://192.168.254.159:8080/yanhuomatou2015.json")
+            //将数据转成字符串
+            val result=url.readText()
+            Log.e("wenzhibin",result)
+            //回传数据给h5,调用js方法必须放在主线程中
+            mContext?.let {
+                it.runOnUiThread {
+                    mWebView?.let { it.loadUrl("javascript:"+callback +
+                            "("+result+")") }
+                }
+            }
+
+
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/winzhibin/androidhtml/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.winzhibin.androidhtml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val mWebView: WebView by lazy {
+        webView
+    }
+    private  val mbtn:Button by lazy {
+        btn
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setWebView()
+       mbtn.onClick {
+           //android调用H5方法
+           var json=JSONObject()
+           json.put("alert","android调用js的showMessage()")
+           mWebView.loadUrl("javascript:showMessage("+json.toString()+")")
+       }
+
+    }
+    var setWebView={
+        //1.开启android与h5通信的开关
+        mWebView.settings.javaScriptEnabled=true
+        //2.设置两个WebViewClinet
+        //2.1设置默认的web浏览器
+        mWebView.webViewClient=MyWebViewClient()
+        //2.2设置chrome浏览器
+        mWebView.webChromeClient=MyWebChromeClient()
+        //设置android与h5通信的桥梁
+        mWebView.addJavascriptInterface(JavaScriptMethods(this,mWebView),"jsInterface") //jsInterface是JavaScriptMethods类的别名
+        //3.加载网页
+        mWebView.loadUrl("http://192.168.254.159:8080/index.html")
+    }
+   inner private class MyWebViewClient:WebViewClient(){
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+        }
+    }
+
+    private class MyWebChromeClient:WebChromeClient(){
+        //加载进度条
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+        }
+
+
+    }
+
+}
